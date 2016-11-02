a077ba5bb5440806374bd7a612994a892b52d9bb
Leon <leon.kwon@uzen.net>
2016-11-02 18:13:21 +0900

    Reverse modifi

--- src/java_fundmental/Reverse.java
+++ src/java_fundmental/Reverse.java
@@ -3,9 +3,15 @@ package java_fundmental;
 
 public class Reverse {
     public String reverse(String str) {
-        // your code here
 
-        return null;
+        if (str == null || str.length() <= 1) {
+            return str;
+        }
+        System.out.println("mid" + str);
+        return reverse(str.substring(1)) + str.charAt(0);
     }
 
+    public static void main(String[] args) {
+        System.out.println(new Reverse().reverse("ABCD"));
+    }
 }
